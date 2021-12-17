8ea666b5e8fb5ddacbb622eb3648f0fb348cb671
Kingdon Barrett <kingdon@weave.works>
2021-12-17 15:31:39 -0500

    for a valid v1 ingress, also include path_type

--- lib/kuby/plugins/rails_app/assets.rb
+++ lib/kuby/plugins/rails_app/assets.rb
@@ -28,6 +28,7 @@ module Kuby
             http do
               path do
                 path spec.asset_url
+                path_type 'Prefix'
 
                 backend do
                   service do
@@ -41,6 +42,7 @@ module Kuby
 
               path do
                 path spec.packs_url
+                path_type 'Prefix'
 
                 backend do
                   service do
